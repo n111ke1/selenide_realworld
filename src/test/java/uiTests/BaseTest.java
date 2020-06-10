927132140e09d463580e24f5711e886d50e0b4fe
n111ke1 <tk.worker15@gmail.com>
2020-06-10 17:23:05 +0300

    added report and parallel run

--- src/test/java/uiTests/BaseTest.java
+++ src/test/java/uiTests/BaseTest.java
@@ -3,8 +3,13 @@ package uiTests;
 import browserProfile.ChromeDriverProvider;
 import browserProfile.FirefoxDriverProvider;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.qameta.allure.selenide.LogType;
 import org.testng.annotations.BeforeMethod;
 import pages.MainPage;
+
+import java.util.logging.Level;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -16,19 +21,24 @@ public class BaseTest {
 
     @BeforeMethod
     public MainPage setUp(){
+        SelenideLogger.addListener("AllureSelenide",
+                new AllureSelenide().screenshots(true)
+                        .savePageSource(true)
+                        .enableLogs(LogType.BROWSER, Level.INFO));
 
-        switch (browserType) {
-            case "chrome":
-                Configuration.browser = ChromeDriverProvider.class.getName();
-                break;
-            case "firefox":
-                Configuration.browser = FirefoxDriverProvider.class.getName();
-                break;
-            default:
-                throw new IllegalArgumentException("Incorrect browse type");
-        }
+//        switch (browserType) {
+//            case "chrome":
+//                Configuration.browser = ChromeDriverProvider.class.getName();
+//                break;
+//            case "firefox":
+//                Configuration.browser = FirefoxDriverProvider.class.getName();
+//                break;
+//            default:
+//                throw new IllegalArgumentException("Incorrect browse type");
+//        }
 
 //        Configuration.headless = true;
+        Configuration.screenshots = true;
         open(baseUrl);
         return new MainPage().checkTittle();
     }
