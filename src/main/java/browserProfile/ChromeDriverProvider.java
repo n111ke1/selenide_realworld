ad6343e6c47d5930a2692f117b79c5d038c2065d
Mykyta Tkalenko <mykyta.tkalenko@artpix3d.com>
2020-06-25 20:27:19 +0300

    fix selenoid url

--- src/main/java/browserProfile/ChromeDriverProvider.java
+++ src/main/java/browserProfile/ChromeDriverProvider.java
@@ -24,7 +24,7 @@ public class ChromeDriverProvider implements WebDriverProvider {
         WebDriver driver = null;
         try {
             driver = new RemoteWebDriver(
-                    URI.create("http://selenoid:4444/wd/hub").toURL(), options);
+                    URI.create("http://localhost:4444/wd/hub").toURL(), options);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
